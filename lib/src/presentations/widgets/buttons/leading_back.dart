60ab0eecf7d54872abd7636f78cf3366d00930ed
Khusan <khukumovkhusan@gmail.com>
2023-05-02 02:48:33 +0200

    Sign Up Screen

---
+++ lib/src/presentations/widgets/buttons/leading_back.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../theme/app_theme.dart';
+
+class LeadingBack extends StatelessWidget {
+  const LeadingBack({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const SizedBox(width: 24),
+        InkWell(
+          borderRadius: BorderRadius.circular(40),
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Container(
+            height: 48,
+            width: 48,
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: AppTheme.white.withOpacity(0.05),
+              borderRadius: BorderRadius.circular(40),
+            ),
+            child: Center(
+              child: SvgPicture.asset(
+                'assets/icons/back.svg',
+                height: 24,
+                width: 24,
+                color: AppTheme.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
